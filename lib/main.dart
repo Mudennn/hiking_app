e294f645d47892ca53be2c50fd3bfd94b7c2899e
Muden <mudennn@yahoo.com>
2023-02-16 17:25:15 +0800

    Gesture Detector

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,6 @@ class HikingApp extends StatefulWidget {
 }
 
 class _HikingAppState extends State<HikingApp> {
- 
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -31,82 +29,90 @@ class FirstPage extends StatefulWidget {
 }
 
 class _FirstPageState extends State<FirstPage> {
-   String buttonName = 'Click';
+  String buttonName = 'Click';
   int currentIndex = 0;
+  bool _isClicked = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Denai Hiking'),
-          backgroundColor: Colors.green[200],
+      appBar: AppBar(
+        title: const Text('Denai Hiking'),
+        backgroundColor: Colors.green[200],
+      ),
+      // body: const Text('Hello Damian!'),
+      body: Center(
+        child: currentIndex == 0
+            ? Container(
+                // untuk width screen
+                width: double.infinity,
+                height: double.infinity,
+                color: Colors.green[50], //background color untuk button
+                child: Row(
+                  // alignment kepada row
+                  mainAxisAlignment: MainAxisAlignment.end, //widget position
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.grey[100],
+                          backgroundColor: Colors.green[500],
+                        ),
+                        onPressed: () {
+                          // setState(() {
+                          //   buttonName = 'Clicked';
+                          // });
+                          // untuk pi ke page lain bukan ikut navbar
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (BuildContext context) {
+                            return const SecondPage();
+                          }));
+                        },
+                        child: Text(buttonName)),
+                    // ElevatedButton(
+                    //     onPressed: () {
+                    //       setState(() {
+                    //         buttonName = 'Clicked';
+                    //       });
+                    //     },
+                    //     child: Text(buttonName)),
+                  ],
+                ),
+              )
+            //  : Image.network("https://unsplash.com/photos/gWjlDDJdmlI"),
+            // : const SizedBox(),
+            : GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _isClicked = !_isClicked;
+                  });
+                },
+                child: _isClicked ? Image.asset('images/tezos.jpg') : null ,
+              ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        backgroundColor: Colors.green[200],
+        selectedIconTheme: IconThemeData(
+          color: Colors.green[500],
         ),
-        // body: const Text('Hello Damian!'),
-        body: Center(
-          child: currentIndex == 0
-              ? Container(
-                  // untuk width screen
-                  width: double.infinity,
-                  height: double.infinity,
-                  color: Colors.green[50], //background color untuk button
-                  child: Row(
-                    // alignment kepada row
-                    mainAxisAlignment: MainAxisAlignment.end, //widget position
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            foregroundColor: Colors.grey[100],
-                            backgroundColor: Colors.green[500],
-                          ),
-                          onPressed: () {
-                            // setState(() {
-                            //   buttonName = 'Clicked';
-                            // });
-                            // untuk pi ke page lain bukan ikut navbar
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (BuildContext context) {
-                              return const SecondPage();
-                            }));
-                          },
-                          child: Text(buttonName)),
-                      // ElevatedButton(
-                      //     onPressed: () {
-                      //       setState(() {
-                      //         buttonName = 'Clicked';
-                      //       });
-                      //     },
-                      //     child: Text(buttonName)),
-                    ],
-                  ),
-                )
-              //  : Image.network("https://unsplash.com/photos/gWjlDDJdmlI"),
-              // : const SizedBox(),
-              : Image.asset('images/tezos.jpg'),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          backgroundColor: Colors.green[200],
-          selectedIconTheme: IconThemeData(
-            color: Colors.green[500],
-          ),
-          unselectedItemColor: Colors.grey[100],
-          items: const [
-            BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
-            BottomNavigationBarItem(label: 'Explore', icon: Icon(Icons.search)),
-            BottomNavigationBarItem(label: 'Trip', icon: Icon(Icons.settings)),
-            BottomNavigationBarItem(label: 'Profile', icon: Icon(Icons.people)),
-          ],
-          // Untuk bagi boleh tukar2 tab
-          currentIndex: currentIndex,
-          onTap: (int index) {
-            setState(() {
-              currentIndex = index;
-            });
-          },
-        ),
+        unselectedItemColor: Colors.grey[100],
+        items: const [
+          BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
+          BottomNavigationBarItem(label: 'Explore', icon: Icon(Icons.search)),
+          BottomNavigationBarItem(label: 'Trip', icon: Icon(Icons.settings)),
+          BottomNavigationBarItem(label: 'Profile', icon: Icon(Icons.people)),
+        ],
+        // Untuk bagi boleh tukar2 tab
+        currentIndex: currentIndex,
+        onTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
     );
   }
 }
