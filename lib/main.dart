7f91c0d418bec917f48053aaee5b4f492429ea1e
Muden <mudennn@yahoo.com>
2023-02-16 16:45:35 +0800

    Navigator v1

--- lib/main.dart
+++ lib/main.dart
@@ -12,54 +12,78 @@ class HikingApp extends StatefulWidget {
 }
 
 class _HikingAppState extends State<HikingApp> {
-  String buttonName = 'Click';
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatefulWidget {
+  const FirstPage({super.key});
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+   String buttonName = 'Click';
   int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
           title: const Text('Denai Hiking'),
           backgroundColor: Colors.green[200],
         ),
         // body: const Text('Hello Damian!'),
         body: Center(
-            child: currentIndex == 0
-                ? Container(
-                    // untuk width screen
-                    width: double.infinity,
-                    height: double.infinity,
-                    color: Colors.green[50], //background color untuk button
-                    child: Row(
-                      // alignment kepada row
-                      mainAxisAlignment:
-                          MainAxisAlignment.end, //widget position
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                foregroundColor: Colors.grey[100],
-                                backgroundColor: Colors.green[500],
-                                ),
-                            onPressed: () {
-                              setState(() {
-                                buttonName = 'Clicked';
-                              });
-                            },
-                            child: Text(buttonName)),
-                        // ElevatedButton(
-                        //     onPressed: () {
-                        //       setState(() {
-                        //         buttonName = 'Clicked';
-                        //       });
-                        //     },
-                        //     child: Text(buttonName)),
-                      ],
-                    ),
-                  )
-                : const SizedBox()),
+          child: currentIndex == 0
+              ? Container(
+                  // untuk width screen
+                  width: double.infinity,
+                  height: double.infinity,
+                  color: Colors.green[50], //background color untuk button
+                  child: Row(
+                    // alignment kepada row
+                    mainAxisAlignment: MainAxisAlignment.end, //widget position
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.grey[100],
+                            backgroundColor: Colors.green[500],
+                          ),
+                          onPressed: () {
+                            // setState(() {
+                            //   buttonName = 'Clicked';
+                            // });
+                            // untuk pi ke page lain bukan ikut navbar
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (BuildContext context) {
+                              return const SecondPage();
+                            }));
+                          },
+                          child: Text(buttonName)),
+                      // ElevatedButton(
+                      //     onPressed: () {
+                      //       setState(() {
+                      //         buttonName = 'Clicked';
+                      //       });
+                      //     },
+                      //     child: Text(buttonName)),
+                    ],
+                  ),
+                )
+              //  : Image.network("https://unsplash.com/photos/gWjlDDJdmlI"),
+              // : const SizedBox(),
+              : Image.asset('images/tezos.jpg'),
+        ),
         bottomNavigationBar: BottomNavigationBar(
           type: BottomNavigationBarType.fixed,
           showSelectedLabels: false,
@@ -83,7 +107,22 @@ class _HikingAppState extends State<HikingApp> {
             });
           },
         ),
-      ),
+    );
+  }
+}
+
+class SecondPage extends StatefulWidget {
+  const SecondPage({super.key});
+
+  @override
+  State<SecondPage> createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
     );
   }
 }
